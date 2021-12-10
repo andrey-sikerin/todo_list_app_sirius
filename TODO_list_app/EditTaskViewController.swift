e69e798bdcfb15e312c70ea56df6c07dae51a14f
Nickolay Mozganov <nick-mozg1@mail.ru>
2021-12-10 21:00:31 +0300

    Add button handlers

--- TODO_list_app/EditTaskViewController.swift
+++ TODO_list_app/EditTaskViewController.swift
@@ -9,28 +9,40 @@ class EditTaskViewController: UIViewController {
         let titleNavigationBarText: String
     }
 
-    init(strings: Strings){
+    init(strings: Strings) {
         self.strings = strings
         super.init(nibName: nil, bundle: nil)
     }
 
-    required init?(coder aDecoder: NSCoder)
-    {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: strings.leftNavigationBarText)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: strings.rightNavigationBarText)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+                title: strings.leftNavigationBarText,
+                style: .plain,
+                target: self,
+                action: #selector(onLeftBarButtonClicked)
+        )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: strings.rightNavigationBarText,
+                style: .plain,
+                target: self,
+                action: #selector(onRightBarButtonClicked)
+        )
         navigationItem.title = strings.titleNavigationBarText
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
+    @objc func onRightBarButtonClicked() {
+        print("Right button pressed")
     }
 
+    @objc func onLeftBarButtonClicked() {
+        print("Left button pressed")
+    }
 }
 

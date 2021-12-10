5dfee3463a47a5d2df3849fc76e3d60f0826cd2d
Nickolay Mozganov <nick-mozg1@mail.ru>
2021-12-10 17:30:05 +0300

    12: Localize strings

--- TODO_list_app/EditTaskViewController.swift
+++ TODO_list_app/EditTaskViewController.swift
@@ -1,33 +1,35 @@
 import UIKit
 
 class EditTaskViewController: UIViewController {
-    let label = UILabel()
+    private let strings: Strings
+
+    public struct Strings {
+        let leftNavigationBarText: String
+        let rightNavigationBarText: String
+        let titleNavigationBarText: String
+    }
+
+    init(strings: Strings){
+        self.strings = strings
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Edit task did load")
-
         view.backgroundColor = .white
 
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save")
-        navigationItem.title = "Thing"
-
-        label.textColor = .black
-        label.text = "EditTaskController"
-        label.textAlignment = .center
-        view.addSubview(label)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: strings.leftNavigationBarText)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: strings.rightNavigationBarText)
+        navigationItem.title = strings.titleNavigationBarText
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-
-        label.frame = CGRect(
-                origin: CGPoint(x: 0, y: 0),
-                size: CGSize(width: 200, height: 200)
-        )
-
-        label.center = view.center
     }
 
 }
--- TODO_list_app/TODO_list_app/RootGraph.swift
+++ TODO_list_app/TODO_list_app/RootGraph.swift
@@ -3,10 +3,14 @@ import UIKit
 
 class RootGraph {
     private(set) var rootViewController: UIViewController
-    
+
     init() {
-        let viewController = EditTaskViewController()
-        let navigationController = NavigationController(rootViewController: viewController)
+        let editTaskViewController = EditTaskViewController(strings: EditTaskViewController.Strings(
+                leftNavigationBarText: NSLocalizedString("Cancel", comment: ""),
+                rightNavigationBarText: NSLocalizedString("Save", comment: ""),
+                titleNavigationBarText: NSLocalizedString("Task", comment: "")
+        ))
+        let navigationController = UINavigationController(rootViewController: editTaskViewController)
         rootViewController = navigationController
     }
 }

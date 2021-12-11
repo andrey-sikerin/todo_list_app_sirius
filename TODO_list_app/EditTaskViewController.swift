bee863a3314d60cba41919ba7f9a975f56cb3d51
Nickolay Mozganov <nick-mozg1@mail.ru>
2021-12-11 17:45:15 +0300

    Add textView

--- TODO_list_app/EditTaskViewController.swift
+++ TODO_list_app/EditTaskViewController.swift
@@ -1,24 +1,47 @@
 import UIKit
 
-class EditTaskViewController: UIViewController {
-
+class EditTaskViewController: UIViewController, UITextViewDelegate {
     private let scrollView = UIScrollView()
-    private let layoutStyle: LayoutStyle = .defaultStyle
-    private let testView = UIView()
+    private let textView = UITextView()
+    private let stackView = UIStackView()
+    private let button = UIButton()
+
+    private let styles: Styles
     private let strings: Strings
+
+    private var showPlaceholder = true
+    private var keyboardHeight: CGFloat = 0
 
     public struct Strings {
         let leftNavigationBarText: String
         let rightNavigationBarText: String
         let titleNavigationBarText: String
+        let textViewPlaceholder: String
+        let buttonText: String
     }
 
-    fileprivate struct LayoutStyle {
-        let contentInsets: UIEdgeInsets
+    public struct Styles {
+        let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 32, right: 16)
+        let itemsBorderRadius: CGFloat
+        let itemsBackgroundColor: UIColor
+        let itemsVerticalMargin: CGFloat = 16
+        let backgroundColor: UIColor
+        let textSize: CGFloat
+
+        let textViewDefaultHeight: CGFloat = 120
+        let textViewInnerPadding: CGFloat = 16
+        let textViewTextColor: UIColor
+        let textViewPlaceholderColor: UIColor
+
+        let stackViewHeight: CGFloat = 113
+        let buttonTextColor: UIColor
+        let buttonPressedTextColor: UIColor
+        let buttonHeight: CGFloat
     }
 
-    init(strings: Strings) {
+    init(strings: Strings, styles: Styles) {
         self.strings = strings
+        self.styles = styles
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -26,9 +49,12 @@ class EditTaskViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTouchScreen))
+        view.addGestureRecognizer(gestureRecognizer)
+        view.backgroundColor = styles.backgroundColor
         navigationItem.leftBarButtonItem = UIBarButtonItem(
                 title: strings.leftNavigationBarText,
                 style: .plain,
@@ -42,17 +68,160 @@ class EditTaskViewController: UIViewController {
                 action: #selector(onRightBarButtonClicked)
         )
         navigationItem.title = strings.titleNavigationBarText
-        testView.backgroundColor = .blue
+
+        // Text View
+        let innerPadding = styles.textViewInnerPadding
+        textView.textContainerInset = UIEdgeInsets(
+                top: innerPadding, left: innerPadding, bottom: innerPadding, right: innerPadding
+        )
+        textView.text = strings.textViewPlaceholder
+        textView.font = .systemFont(ofSize: styles.textSize)
+        textView.layer.cornerRadius = styles.itemsBorderRadius
+        textView.backgroundColor = styles.itemsBackgroundColor
+        textView.textColor = styles.textViewPlaceholderColor
+        textView.isScrollEnabled = false
+        textView.delegate = self
+
+        // Stack Views
+        stackView.autoresizingMask = []
+        stackView.layer.cornerRadius = styles.itemsBorderRadius
+        stackView.backgroundColor = styles.itemsBackgroundColor
+
+        // Button
+        button.addTarget(self, action: #selector(onButtonPress), for: .touchUpInside)
+        button.layer.cornerRadius = styles.itemsBorderRadius
+        button.backgroundColor = styles.itemsBackgroundColor
+        button.setTitleColor(styles.buttonTextColor, for: .normal)
+        button.setTitleColor(styles.buttonPressedTextColor, for: .highlighted)
+        button.titleLabel?.font = .systemFont(ofSize: styles.textSize)
+        button.setTitle(strings.buttonText, for: .normal)
+
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.addSubview(textView)
+        scrollView.addSubview(stackView)
+        scrollView.addSubview(button)
         view.addSubview(scrollView)
-        scrollView.addSubview(testView)
+
+        NotificationCenter.default.addObserver(self,
+                selector: #selector(onKeyboardOpened(keyboardShowNotification:)),
+                name: UIResponder.keyboardDidShowNotification,
+                object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                selector: #selector(onKeyboardClosed(keyboardShowNotification:)),
+                name: UIResponder.keyboardDidHideNotification,
+                object: nil)
+    }
+
+    @objc func onKeyboardOpened(keyboardShowNotification notification: Notification) {
+        print("Keyboard opened")
+        changeTextViewSize(canStretchIndefinitely: false, withAnimation: true)
+
+        if let userInfo = notification.userInfo,
+           let keyboardRectangle = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            keyboardHeight = keyboardRectangle.height
+        }
+    }
+
+    @objc func onButtonPress() {
+        print("Button pressed")
+    }
+
+    @objc func onKeyboardClosed(keyboardShowNotification notification: Notification) {
+        print("Keyboard closed")
+        changeTextViewSize(canStretchIndefinitely: true, withAnimation: true)
+        setItemsLayout()
+    }
+
+    @objc func onTouchScreen(_ sender: UITapGestureRecognizer) {
+        print("Screen touched")
+        textView.resignFirstResponder()
     }
 
     @objc func onRightBarButtonClicked() {
+        textView.resignFirstResponder()
         print("Right button pressed")
     }
 
     @objc func onLeftBarButtonClicked() {
         print("Left button pressed")
+    }
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("Begin editing")
+        textView.textColor = styles.textViewTextColor
+        if (showPlaceholder) {
+            textView.text = ""
+            textView.textColor = styles.textViewTextColor
+        }
+    }
+
+
+    private func changeTextViewSize(canStretchIndefinitely: Bool, withAnimation: Bool = false) {
+        let textViewAvailableHeight = view.frame.height - scrollView.contentInset.bottom -
+                scrollView.contentInset.top - keyboardHeight - 2 * styles.itemsVerticalMargin
+
+        let fixedWidth = textView.frame.size.width
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        let currentTextViewHeight = max(newSize.height, styles.textViewDefaultHeight)
+
+        var height: CGFloat
+        if currentTextViewHeight > textViewAvailableHeight && !canStretchIndefinitely {
+            print("Text view doesn't have enough space, start scrolling")
+            height = textViewAvailableHeight
+            textView.isScrollEnabled = true
+        } else {
+            print("Text view has enough space, extends")
+            textView.isScrollEnabled = false
+            height = currentTextViewHeight
+        }
+
+        newFrame.size = CGSize(
+                width: max(newSize.width, fixedWidth),
+                height: height
+        )
+        newFrame.origin = .zero
+
+        UIView.animateKeyframes(
+                withDuration: withAnimation ? 0.5 : 0,
+                delay: 0,
+                options: [],
+                animations: {
+                    self.textView.frame = newFrame
+                }, completion: nil)
+        textView.scrollRangeToVisible(textView.selectedRange)
+        setItemsLayout()
+    }
+
+
+    func textViewDidChange(_ textView: UITextView) {
+        changeTextViewSize(canStretchIndefinitely: false)
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("End editing")
+        if textView.text.isEmpty {
+            showPlaceholder = true
+            textView.text = strings.textViewPlaceholder
+            textView.textColor = styles.textViewPlaceholderColor
+        } else {
+            showPlaceholder = false
+        }
+    }
+
+
+    private func setItemsLayout() {
+        stackView.frame = CGRect(
+                origin: CGPoint(x: 0, y: textView.bounds.height + styles.itemsVerticalMargin),
+                size: CGSize(width: scrollView.contentSize.width, height: styles.stackViewHeight)
+        )
+
+        let buttonOriginY = stackView.frame.maxY + styles.itemsVerticalMargin
+        button.frame = CGRect(
+                origin: CGPoint(x: 0, y: buttonOriginY),
+                size: CGSize(width: scrollView.contentSize.width, height: styles.buttonHeight)
+        )
     }
 
     override func viewWillLayoutSubviews() {
@@ -61,24 +230,27 @@ class EditTaskViewController: UIViewController {
                 height: view.frame.height)
         var customSafeAreaInsets = view.safeAreaInsets
         customSafeAreaInsets.top = 0
-        let sumInsets = customSafeAreaInsets + layoutStyle.contentInsets
+        let sumInsets = customSafeAreaInsets + styles.contentInsets
 
         scrollView.contentInset = sumInsets
         scrollView.contentSize = CGSize(
                 width: view.frame.width - sumInsets.left - sumInsets.right,
                 height: 2 * view.frame.height
         )
-        testView.frame = CGRect(x: .zero, y: .zero, width: scrollView.contentSize.width, height: scrollView.contentSize.height)
+
+        let itemsWidth = scrollView.contentSize.width
+
+        textView.frame = CGRect(
+                origin: .zero,
+                size: CGSize(width: itemsWidth, height: styles.textViewDefaultHeight)
+        )
+        setItemsLayout()
     }
 }
 
-fileprivate extension UIEdgeInsets{
-    static func + (lhs: UIEdgeInsets, rhs: UIEdgeInsets) -> UIEdgeInsets {
+fileprivate extension UIEdgeInsets {
+    static func +(lhs: UIEdgeInsets, rhs: UIEdgeInsets) -> UIEdgeInsets {
         UIEdgeInsets(top: lhs.top + rhs.top, left: lhs.left + rhs.left, bottom: lhs.bottom + rhs.bottom, right: lhs.right + rhs.right)
     }
-}
-
-fileprivate extension EditTaskViewController.LayoutStyle {
-    static let defaultStyle = Self.init(contentInsets: UIEdgeInsets(top: 16, left: 16, bottom: 32, right: 16))
 }
 
--- TODO_list_app/TODO_list_app/RootGraph.swift
+++ TODO_list_app/TODO_list_app/RootGraph.swift
@@ -5,11 +5,26 @@ class RootGraph {
     private(set) var rootViewController: UIViewController
 
     init() {
-        let editTaskViewController = EditTaskViewController(strings: EditTaskViewController.Strings(
-                leftNavigationBarText: NSLocalizedString("Cancel", comment: ""),
-                rightNavigationBarText: NSLocalizedString("Save", comment: ""),
-                titleNavigationBarText: NSLocalizedString("Task", comment: "")
-        ))
+        let editTaskViewController = EditTaskViewController(
+                strings: EditTaskViewController.Strings(
+                        leftNavigationBarText: NSLocalizedString("Cancel", comment: ""),
+                        rightNavigationBarText: NSLocalizedString("Save", comment: ""),
+                        titleNavigationBarText: NSLocalizedString("Task", comment: ""),
+                        textViewPlaceholder: NSLocalizedString("TaskDescriptionPlaceholder", comment: ""),
+                        buttonText: NSLocalizedString("Delete", comment: "")
+                ),
+                styles: EditTaskViewController.Styles(
+                        itemsBorderRadius: AppStyles.borderRadius,
+                        itemsBackgroundColor: .white,
+                        backgroundColor: .lightGray,
+                        textSize: 17,
+                        textViewTextColor: .black,
+                        textViewPlaceholderColor: .gray,
+                        buttonTextColor: .gray,
+                        buttonPressedTextColor: .black,
+                        buttonHeight: 56
+                )
+        )
         let navigationController = UINavigationController(rootViewController: editTaskViewController)
         rootViewController = navigationController
     }

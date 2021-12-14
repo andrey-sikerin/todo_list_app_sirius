cda7fc63bd1b18129b7cff9af7e8f6876e301b64
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2021-12-14 14:51:08 +0300

    Added Colors

---
+++ TODO_list_app/Color.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+Assets.swift
+//  TODO_list_app
+//
+//  Created by Artem Goldenberg on 14.12.2021.
+//
+
+import Foundation
+import UIKit
+
+final class Color {
+    static var backgroundElevated: UIColor { UIColor(named: #function)! }
+    static var backgroundIOSPrimary: UIColor { UIColor(named: #function)! }
+    static var backgroundPrimary: UIColor { UIColor(named: #function)! }
+    static var backgroundSecondary: UIColor { UIColor(named: #function)! }
+    
+    static var blue: UIColor { UIColor(named: #function)! }
+    static var red: UIColor { UIColor(named: #function)! }
+    static var gray: UIColor { UIColor(named: #function)! }
+    static var green: UIColor { UIColor(named: #function)! }
+    static var white: UIColor { UIColor(named: #function)! }
+    static var lightGray: UIColor { UIColor(named: #function)! }
+    
+    static var labelDisable: UIColor { UIColor(named: #function)! }
+    static var labelPrimary: UIColor { UIColor(named: #function)! }
+    static var labelSecondary: UIColor { UIColor(named: #function)! }
+    static var labelTertiary: UIColor { UIColor(named: #function)! }
+    
+    static var navBarBlur: UIColor { UIColor(named: #function)! }
+    static var overlay: UIColor { UIColor(named: #function)! }
+    static var separator: UIColor { UIColor(named: #function)! }
+}

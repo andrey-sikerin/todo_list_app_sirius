068942b0c9c30944dcb6e44e38f5a8d956ed0948
Andrey Sikerin <asikerin@yandex-team.ru>
2021-12-17 18:22:46 +0300

    Implement show/hide semantics

--- TODO_list_app/ListTasks/Models/HeaderViewModel.swift
+++ TODO_list_app/ListTasks/Models/HeaderViewModel.swift
@@ -7,20 +7,43 @@ import RxSwift
 import RxCocoa
 
 class HeaderViewModel {
+    struct Strings {
+      let doneText: String
+      let hideText: String
+      let showText: String
+    }
+    enum Mode {
+      case show
+      case hide
+    }
+    let observableDoneAmount : Observable<String>
+    let observableButtonText: Observable<String>
 
-    private let subjectDoneAmount: PublishSubject<Int>
-    private let subjectButtonText: PublishSubject<String>
-    var observableDoneAmount : Observable<Int>  {
-        subjectDoneAmount
-    }
-    var observableButtonText: Observable<String> {
-        subjectButtonText
-    }
-    init() {
-        subjectDoneAmount = PublishSubject()
-        subjectDoneAmount.onNext(0)
-        subjectButtonText = PublishSubject()
-        subjectButtonText.onNext(NSLocalizedString("Show", comment: ""))
+    private let todoItemsObservable: Observable<[TodoItem]>
+    private let mode : BehaviorSubject<Mode>
+    init(
+      todoItemsObservable: Observable<[TodoItem]>,
+      strings: Strings,
+      modeSubject: BehaviorSubject<Mode>
+    ) {
+        self.todoItemsObservable = todoItemsObservable
+        self.mode = modeSubject
+        observableDoneAmount = todoItemsObservable.map { items in
+          return "\(strings.doneText) - \(items.filter({ $0.done }).count)"
+        }
+
+      observableButtonText = mode.map { mode in
+          switch mode {
+          case .show:
+            return strings.showText
+          case .hide:
+            return strings.hideText
+          }
+      }
     }
 
+    func doneButtonPressed() {
+      let value = try! mode.value()
+      mode.onNext(value == .hide ? .show : .hide)
+    }
 }
